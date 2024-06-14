839f1d88fbee745804f807b8cf6c35e8126c6fcc
Cjc <183237512@qq.com>
2024-06-14 23:00:55 +0800

    09-6-14-23:00

---
+++ docs/assets/yaliIndex-u56fIlnG.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,q as n}from"./index-Bf67MVQv.js";const o={};function r(t,a){return c(),n("div",null," 压力计 ")}const _=e(o,[["render",r]]);export{_ as default};
--- src/views/theHome.vue
+++ src/views/theHome.vue
@@ -7,12 +7,51 @@
     </template>
     <template #default>
       <div style="display: flex; justify-content:space-around ">
-        <img src="@/assets/images/cd.png" alt="">
-        <toolItems text="地层柱状图绘制" imgUrl="../assets/images/cd.png" path="/dc"></toolItems>
-        <toolItems text="双压力计抽水试验" imgUrl="../assets/images/yali.png" path="/yali"></toolItems>
+        <RouterLink to="/dc" style="text-decoration: none;">
+          <div class="ele" style="width: 400px;height: 450px;">
+            <p class="text">地层柱状图绘制</p>
+            <img ref="imgRef" class="img" src="../assets/images/cd.png" />
+          </div>
+        </RouterLink>
+        <RouterLink to="/yali" style="text-decoration: none;">
+          <div class="ele" style="width: 400px;height: 450px;">
+            <p class="text">双压力计抽水试验</p>
+            <img ref="imgRef" class="img" src="../assets/images/yali.png" />
+          </div>
+        </RouterLink>
       </div>
     </template>
   </el-card>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.ele:hover {
+  //悬浮
+  transform: scale(1.03);
+  transition: all 0.5s;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  border-radius: 5px;
+
+  .img {
+    border-radius: 0 0 5px 5px;
+  }
+}
+
+.text {
+  width: 100%;
+  height: 10%;
+  line-height: 50px;
+  font-size: 25px;
+  text-align: center;
+  color: black;
+}
+
+.img {
+  width: 100%;
+  height: 90%;
+  background-size: cover;
+  background-position: right;
+  background-repeat: no-repeat;
+  border: 3px solid black;
+}
+</style>
